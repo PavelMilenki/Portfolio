62018218d093c842d630ce4528302b9fe0c9734b
Pavel Milenki <p.milenkii@gmail.com>
2019-10-15 13:28:00 +0300

    Updates

---
+++ precache-manifest.75ad4ddb3d330efa39f4d220c25e6a3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1f1f1c448e76c62bff4364207148d92",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "0692c6f18d20f6672676",
+    "url": "/Portfolio/static/css/main.63dd0656.chunk.css"
+  },
+  {
+    "revision": "30769c8b09891ced0d04",
+    "url": "/Portfolio/static/js/2.abdd172b.chunk.js"
+  },
+  {
+    "revision": "0692c6f18d20f6672676",
+    "url": "/Portfolio/static/js/main.ddac6222.chunk.js"
+  },
+  {
+    "revision": "13c12ee1e289ad549380",
+    "url": "/Portfolio/static/js/runtime~main.ff9ece22.js"
+  }
+]);
