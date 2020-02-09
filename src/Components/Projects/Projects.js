38a927d33e914d1a198a922e1c54766d9da1ed3b
Pavel Milenki <54291798+PavelMilenki@users.noreply.github.com>
2020-02-09 10:01:22 +0300

    added fix styles

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -4,8 +4,7 @@ import {Project} from "./Project";
 import network from "../../Helpers/Images/network.png"
 import todolist from "../../Helpers/Images/Todolist.jpg"
 import booksStore from "../../Helpers/Images/BooksStore.jpg"
-import counter from "../../Helpers/Images/counter.png"
-
+import shortLink from "../../Helpers/Images/shortLink.jpg"
 
 
 export const Projects = () => {
@@ -13,11 +12,12 @@ export const Projects = () => {
     const projects = [
         {
             id: 1,
-            title: 'Social network',
-            img: network,
-            link: 'https://pavelmilenki.github.io/SocialNetwork/',
-            githubCode: 'https://github.com/PavelMilenki/SocialNetwork'
-                    },
+            title: 'To do list',
+            img: todolist,
+            link: 'https://pavelmilenki.github.io/ToDoList/',
+            githubCode: 'https://github.com/PavelMilenki/ToDoList'
+        },
+
         {
             id: 2,
             title: 'Books store',
@@ -27,37 +27,38 @@ export const Projects = () => {
         },
         {
             id: 3,
-            title: 'To do list',
-            img: todolist,
-            link: 'https://pavelmilenki.github.io/ToDoList/',
-            githubCode: 'https://github.com/PavelMilenki/ToDoList'
+            title: 'Social network',
+            img: network,
+            link: 'https://pavelmilenki.github.io/SocialNetwork/',
+            githubCode: 'https://github.com/PavelMilenki/SocialNetwork'
         },
         {
             id: 4,
-            title: 'Counter',
-            img: counter,
-            link: 'https://pavelmilenki.github.io/CounterWithRedux/',
-            githubCode: 'https://github.com/PavelMilenki/CounterWithRedux'
+            title: 'Shortener link',
+            img: shortLink,
+            link: 'https://shortlinksapp.herokuapp.com/',
+            githubCode: 'https://github.com/PavelMilenki/shortener-link'
         }
     ];
 
     return (
         <div className={s.projects} id={'works'}>
-                      <div className={s.container}>
-                    <h2>web developer portfolio</h2>
-                    <p>From Web Components  to React.JS, Redux and Node.JS. Check out my latest web software development portfolio projects.</p>
-                    <div className={s.projectsWrapper}>
-                        {projects.map(p => {
-                            return <Project key={p.id}
-                                            title={p.title}
-                                            img={p.img}
-                                            link={p.link}
-                                            githubCode={p.githubCode}
-                                            description={p.description}/>
-                        })}
-                    </div>
+            <div className={s.container}>
+                <h2>web developer portfolio</h2>
+                <p>From Web Components to React.JS, Redux and Node.JS. Check out my latest web software development
+                    portfolio projects.</p>
+                <div className={s.projectsWrapper}>
+                    {projects.map(p => {
+                        return <Project key={p.id}
+                                        title={p.title}
+                                        img={p.img}
+                                        link={p.link}
+                                        githubCode={p.githubCode}
+                                        description={p.description}/>
+                    })}
                 </div>
-                </div>
+            </div>
+        </div>
     );
 };
 
